21253f66fb98c9776a2925e6263249366bb802f7
Mayuri Sure <141281357+Mayuri1975@users.noreply.github.com>
2026-01-16 15:20:21 +0530

    Fix case-sensitive import for Signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 
 import "./index.css";
-import Signup from './pages/signup';
+import Signup from './pages/Signup';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
